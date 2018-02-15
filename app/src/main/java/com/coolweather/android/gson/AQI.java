fadfd1d6b851f7e4a82fb517ae6ab794ef64d878
Cloud <347612492@qq.com>
2018-02-15 15:03:33 +0800

    加入显示天气信息和手动下拉更新功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,18 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Cloud on 2018/2/14.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+}
